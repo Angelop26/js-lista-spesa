b269713b0cfdf33416f6cad5e9d18d9e925cb13e
Angelop26 <angeloprospero26@gmail.com>
2023-03-15 14:05:51 +0100

    project complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-alert('ciao')
+const ul = document.querySelector('ul')
+let listaSpesa = ''
+i = 0
+while (i < 10) {
+    const elem = prompt('dimmi cosa aggiungere alla lista della spesa')
+    listaSpesa += `<li>${elem}</li>`
+    i++
+}
+
+ul.innerHTML = listaSpesa
